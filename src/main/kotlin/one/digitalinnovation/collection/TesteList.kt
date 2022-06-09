8bf2ceeb5bb8e0086f2077b45a91d5f955dd352d
iamjaypierre <jepiierre@gmail.com>
2022-06-09 13:32:27 -0300

    created a test list class

---
+++ src/main/kotlin/one/digitalinnovation/collection/TesteList.kt
@@ -0,0 +1,43 @@
+package one.digitalinnovation.collection
+
+fun main() {
+
+    val joao = Funcionario("Joao", 2000.0, "CLT")
+    val pedro = Funcionario("Pedro", 1000.0, "PJ")
+    val maria = Funcionario("Maria", 4000.0, "CLT")
+
+    val funcionarios = listOf(joao, pedro, maria)
+
+    funcionarios.forEach { println(it) }
+
+
+    println("---------------------------")
+    println(funcionarios.find { it.nome == "Maria" })
+
+    println("---------------------------")
+    funcionarios.sortedBy { it.salario }
+        .forEach { println(it) }
+
+    println("---------------------------")
+    funcionarios.groupBy { it.tipoContratacao }
+        .forEach { println(it) }
+
+
+
+}
+
+
+data class  Funcionario(
+    val nome: String,
+    val salario: Double,
+    val tipoContratacao: String
+) {
+    override fun toString(): String =
+        """
+            Nome:    $nome
+            Salario: $salario
+        """.trimIndent()
+
+
+}
+
