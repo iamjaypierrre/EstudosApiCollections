789463aa37d58c80e5240f751706241b68a4126b
iamjaypierre <jepiierre@gmail.com>
2022-06-09 12:57:05 -0300

    created a operation  test class

--- src/main/kotlin/one/digitalinnovation/collection/TesteOperacoes.kt
+++ src/main/kotlin/one/digitalinnovation/collection/TesteOperacoes.kt
@@ -8,13 +8,26 @@ fun main() {
     }
 
     println("----------------------")
-    //.max nao funciona println("Maior salario: ${salarios.max}")
-    //.min nao funciona println("Menor salario: ${salarios.min}")
+    println("Maior salario: ${salarios.maxOrNull()}")
+    println("Menor salario: ${salarios.minOrNull()}")
     println("Media salarial: ${salarios.average()}")
 
     val salariosMaiorQue2500 = salarios.filter { it > 2500.0 }
     println("-----------------")
     salariosMaiorQue2500.forEach { println(it) }
+
+    println("----------------------")
+    println(salarios.find {it == 2250.0 })
+    println(salarios.find {it == 500.0 })
+
+    println("----------------------")
+    println(salarios.any {it == 1000.0 })
+    println(salarios.any {it == 500.0 })
+
+
+
+
+
 
 
 }
